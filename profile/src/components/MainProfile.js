4486b81164ed28c1811e8f41522b2eae329a1091
Brian Ball <brianbll9@gmail.com>
2019-02-24 10:44:32 -0500

    Create MainProfile.js

---
+++ profile/src/components/MainProfile.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import {Image, Col, Container} from 'react-bootstrap'
+
+export default class MainProfile extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            profile: null
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <Container>
+                    <Col xs={6} md={4}>
+                        <Image src={this.props.profile.gravatar_url} roundedCircle />
+                    </Col>
+                </Container>
+
+                <h1>{this.props.profile.name}</h1>
+                <h3>{this.props.profile.profile_name}</h3>
+                
+
+            </div>
+
+        )
+    }
+
+}
